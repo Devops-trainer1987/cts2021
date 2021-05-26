0e5196da0e98ed2122022924b3addb34cde09884
Devops-trainer1987 <60420970+Devops-trainer1987@users.noreply.github.com>
2021-05-26 12:09:15 +0530

    Update App.java

--- src/main/java/cts/App.java
+++ src/main/java/cts/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello maven!" );
+        System.out.println( "Hello jenkinssss....." );
     }
 }
